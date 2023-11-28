645953c538c2dcbb4b87a9334bb73f262be161af
cessdaniels <musyokicicilia3@gmail.com>
2023-11-28 00:52:00 -0800

    linked files

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -19,8 +19,8 @@ typedef struct listint_s
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
-void free_listint(*list);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
-#endif /* LISTS_H */listint_t *head);
-int check_cycle(listint_t 
+#endif /* LISTS_H */
 
